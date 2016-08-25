79dc2ba95b9ced8cfa3ce93c55eb6c77fd3262d7
Richard Kopelow <richardkopelow@gmail.com>
2016-08-25 10:35:58 -0400

    Added roll command

--- RedBot/Commands/Utility/UtilityCommands.cs
+++ RedBot/Commands/Utility/UtilityCommands.cs
@@ -58,6 +58,44 @@ namespace RedBot.Commands
 					await e.Server.TextChannels.FirstOrDefault(channel => channel.Name == "eboard")?.SendMessage(String.Format("{0}, {1} is at the door!", e.Server.Roles.FirstOrDefault(channel => channel.Name == "eboard")?.Mention, e.User.Name));
 					await e.User.SendMessage("The eboard has been alerted!");
 				});
-		}
+
+            cmd.CreateCommand("roll")
+                .Description("Rolls a die")
+                .Parameter("modifier", ParameterType.Optional)
+                .Do(async e =>
+                {
+                    Random rand = new Random();
+                    string die = "d100";
+                    if (e.Args.Length > 1)
+                    {
+                        die = e.Args[0];
+                    }
+                    string[] parts = die.Split('d');
+                    if (parts.Length < 2)
+                    {
+                        await e.Channel.SendMessage("Improper dice");
+                        return;
+                    }
+                    parts[0] = parts[0] == "" ? "1" : parts[0];
+                    int rolls = 0;
+                    int sides = 0;
+                    if (!int.TryParse(parts[0], out rolls))
+                    {
+                        await e.Channel.SendMessage("Improper dice");
+                        return;
+                    }
+                    if (int.TryParse(parts[1], out sides))
+                    {
+                        await e.Channel.SendMessage("Improper dice");
+                        return;
+                    }
+                    int sum = 0;
+                    for (int i = 0; i < rolls; i++)
+                    {
+                        sum += rand.Next(1, sides + 1);
+                    }
+                    await e.Channel.SendMessage(sum.ToString());
+                });
+        }
 	}
 }
